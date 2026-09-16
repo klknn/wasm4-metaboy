ab7904c89e266378600025014bf3ed9a317b1b17
klknn <klknn.gh@gmail.com>
2026-09-17 01:41:12 +0900

    docs(mmu): add memory map references and use bitfields for IF and joypad

--- source/gb/mmu.d
+++ source/gb/mmu.d
@@ -1,8 +1,41 @@
+/**
+ * Game Boy Memory Management Unit (MMU) & Memory Banking Controller (MBC)
+ *
+ * Pan Docs References:
+ * - Memory Map:  https://gbdev.io/pandocs/Memory_Map.html
+ * - MBC1:        https://gbdev.io/pandocs/MBC1.html
+ * - Joypad:      https://gbdev.io/pandocs/Joypad_Input.html
+ * - Interrupts:  https://gbdev.io/pandocs/Interrupts.html
+ *
+ * ----------------------------------------------------------------------------
+ * Game Boy 16-bit (64 KiB) Address Map:
+ * ----------------------------------------------------------------------------
+ * 0x0000 - 0x3FFF | 16 KiB ROM Bank 00        | From cartridge, usually non-switchable
+ * 0x4000 - 0x7FFF | 16 KiB Switchable ROM Bank | Bank 01..NN switchable via MBC
+ * 0x8000 - 0x9FFF |  8 KiB Video RAM (VRAM)   | Tile data & tilemaps (managed by PPU)
+ * 0xA000 - 0xBFFF |  8 KiB External Cart RAM   | Cartridge SRAM (battery backed in saves)
+ * 0xC000 - 0xCFFF |  4 KiB Work RAM (WRAM) B0  | Internal system RAM
+ * 0xD000 - 0xDFFF |  4 KiB Work RAM (WRAM) B1  | Internal system RAM
+ * 0xE000 - 0xFDFF |  Echo RAM (prohibited)     | Mirrors 0xC000..0xDDFF
+ * 0xFE00 - 0xFE9F |  Sprite OAM (160 bytes)    | 40 sprite attributes (managed by PPU)
+ * 0xFEA0 - 0xFEFF |  Unusable Memory           | Reads 0xFF, writes ignored
+ * 0xFF00 - 0xFF7F |  I/O Registers             | Joypad, Timer, Serial, PPU, Audio, DMA
+ * 0xFF80 - 0xFFFE |  High RAM (HRAM) (127 B)   | Fast zero-page RAM
+ * 0xFFFF          |  Interrupt Enable (IE)     | 1 byte interrupt mask
+ * ----------------------------------------------------------------------------
+ *
+ * MBC1 Banking Registers:
+ * - 0x0000 - 0x1FFF: RAM Enable (writing 0x0A enables RAM, any other value disables)
+ * - 0x2000 - 0x3FFF: ROM Bank Number (lower 5 bits: 1..31; 0 is translated to 1)
+ * - 0x4000 - 0x5FFF: RAM Bank Number (0..3) or Upper 2 bits of ROM Bank (bits 5-6)
+ * - 0x6000 - 0x7FFF: Banking Mode Select (0 = ROM banking mode, 1 = RAM banking mode)
+ */
 module gb.mmu;
 
 import gb.types;
 import gb.ppu;
 import gb.timer;
+import std.bitmanip : bitfields;
 import w4 = wasm4;
 
 struct MMU {
@@ -16,7 +49,7 @@ struct MMU {
     u8   cartType = 0;
     u8   romBank5 = 1;       // 5-bit ROM bank register (1..31)
     u8   ramBank = 0;        // 2-bit RAM bank / upper ROM bank register (0..3)
-    u8   bankingMode = 0;    // 0: ROM banking mode, 1: RAM banking mode
+    u8   bankingMode = 0;    // 0: ROM banking mode (up to 2MB ROM, 8KB RAM), 1: RAM banking mode
     bool ramEnabled = false;
     uint numRomBanks = 2;
     uint numRamBanks = 0;
@@ -24,17 +57,39 @@ struct MMU {
     u8[8192] wram;      // 0xC000 - 0xDFFF (Work RAM)
     u8[128]  hram;      // 0xFF80 - 0xFFFE (High RAM)
 
-    u8 iflag = 0xE1;    // 0xFF0F Interrupt Flag
-    u8 ie    = 0x00;    // 0xFFFF Interrupt Enable
+    // 0xFF0F: Interrupt Flag (IF) - active interrupt requests
+    // Pan Docs: https://gbdev.io/pandocs/Interrupts.html#ff0f--if-interrupt-flag
+    union {
+        u8 iflag = 0xE1;
+        mixin(bitfields!(
+            bool, "ifVBlank", 1, // Bit 0: VBlank interrupt request
+            bool, "ifStat",   1, // Bit 1: LCD STAT interrupt request
+            bool, "ifTimer",  1, // Bit 2: Timer interrupt request
+            bool, "ifSerial", 1, // Bit 3: Serial interrupt request
+            bool, "ifJoypad", 1, // Bit 4: Joypad interrupt request
+            uint, "_padIF",   3  // Bits 5-7: Unused, always read as 1 on DMG
+        ));
+    }
 
-    // Joypad state (0xFF00)
-    u8 joypSelect = 0x30;
-    u8 joypadButtons = 0xFF; // Down, Up, Left, Right (active low)
-    u8 joypadActions = 0xFF; // Start, Select, B, A (active low)
+    u8 ie    = 0x00;    // 0xFFFF: Interrupt Enable (IE) mask
 
-    // Serial
-    u8 sb = 0x00;       // 0xFF01
-    u8 sc = 0x7E;       // 0xFF02
+    // 0xFF00: Joypad register (P1 / JOYP)
+    // Pan Docs: https://gbdev.io/pandocs/Joypad_Input.html
+    union {
+        u8 joypSelect = 0x30;
+        mixin(bitfields!(
+            uint, "_padJoyLower",   4, // Bits 0-3: Read-only button states
+            bool, "selectDirection", 1, // Bit 4: 0 = select directional keys
+            bool, "selectAction",    1, // Bit 5: 0 = select action buttons
+            uint, "_padJoyUpper",   2  // Bits 6-7: Unused, always 1 on DMG
+        ));
+    }
+    u8 joypadButtons = 0xFF; // Down, Up, Left, Right (active low: 0 = pressed)
+    u8 joypadActions = 0xFF; // Start, Select, B, A (active low: 0 = pressed)
+
+    // Serial Data Transfer
+    u8 sb = 0x00;       // 0xFF01: Serial transfer data
+    u8 sc = 0x7E;       // 0xFF02: Serial transfer control
 
     PPU   ppu;
     Timer timer;
@@ -141,8 +196,8 @@ struct MMU {
             return 0xFF;
         } else if (addr == 0xFF00) { // Joypad
             u8 res = joypSelect | 0xCF;
-            if (!(joypSelect & 0x10)) res &= (joypadButtons | 0xF0);
-            if (!(joypSelect & 0x20)) res &= (joypadActions | 0xF0);
+            if (!selectDirection) res &= (joypadButtons | 0xF0);
+            if (!selectAction)    res &= (joypadActions | 0xF0);
             return res;
         } else if (addr == 0xFF01) {
             return sb;
@@ -309,6 +364,14 @@ unittest {
     // Switch back to RAM bank 0
     mmu.write(0x4000, 0x00);
     assert(mmu.read(0xA000) == 0x77);
+
+    // Test IF and Joypad bitfield getters
+    mmu.iflag = 0xE1;
+    assert(mmu.ifVBlank);
+    assert(!mmu.ifTimer);
+    mmu.write(0xFF00, 0x20); // selectDirection=0, selectAction=1
+    assert(!mmu.selectDirection);
+    assert(mmu.selectAction);
 
     import core.stdc.stdio : printf;
     printf("✔ [MMU] Unittests passed.\n");
